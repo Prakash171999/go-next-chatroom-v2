f817acdb9bbe4f83616412c57ec5aa6e5888bf17
Prakash Rai <prakash.bahing21@gmail.com>
2023-04-12 12:57:36 +0545

    Feat:Websocket connection

---
+++ client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  4096,
+	WriteBufferSize: 4096,
+}
+
+type Client struct {
+	conn *websocket.Conn
+}
+
+func newClient(conn *websocket.Conn) *Client {
+	return &Client{
+		conn: conn,
+	}
+}
+
+func ServeWs(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Panicln(err)
+		return
+	}
+
+	client := newClient(conn)
+
+	fmt.Println("New client joined!")
+	fmt.Println(client)
+}
